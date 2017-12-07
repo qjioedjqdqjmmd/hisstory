664d1e50b97810d72115bfe0b68891310b85f755
qjioedjqdqjmmd <34190387+qjioedjqdqjmmd@users.noreply.github.com>
2017-12-08 00:35:57 +0900

    npm google api 추가

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
-var express = require('express');
+var google = require('googleapis');
 
-var app = express();
+var blogger = google.blogger('v3');
+var util = require('util');
 
+var _KEY = '';
+var _BLOGID = '';
+
+blogger.blogs.get({
+  key: _KEY,
+  blogId: _BLOGID
+}, function (err, result) {
+  if (err) {
+    console.error(err);
+  }
+  if (result) {
+    console.log(util.inspect(result));
+  }
+});
