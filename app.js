f44c30e2e487d42b40aedfc1627df04012433195
qjioedjqdqjmmd <34190387+qjioedjqdqjmmd@users.noreply.github.com>
2017-12-25 20:43:45 +0900

    모듈추가
    auth 연습
    스크린샷 등

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 var google = require('googleapis');
+var webshot = require('webshot');
+
+var request = require('request');
 
 var blogger = google.blogger('v3');
 var util = require('util');
@@ -6,6 +9,7 @@ var util = require('util');
 var _KEY = '';
 var _BLOGID = '';
 
+/*
 blogger.blogs.get({
   key: _KEY,
   blogId: _BLOGID
@@ -16,4 +20,88 @@ blogger.blogs.get({
   if (result) {
     console.log(util.inspect(result));
   }
+});*/
+
+/*
+var shot_options = {
+  screenSize: {
+    width: 1024
+  , height: 768
+  }
+, shotSize: {
+    width: 'all'
+  , height: 'all'
+  }
+};
+webshot('https://datalab.naver.com/', './dest/naver-result.png', shot_options, function(err) {
+  // screenshot now saved to google.png 
 });
+*/
+var readline = require('readline');
+var OAuth2Client = google.auth.OAuth2;
+var CLIENT_ID = '';
+var CLIENT_SECRET = '';
+var oauth2Client = new OAuth2Client(CLIENT_ID, CLIENT_SECRET);
+
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function getAccessToken (oauth2Client, callback) {
+  // generate consent page url
+  var url = oauth2Client.generateAuthUrl({
+    access_type: 'offline', // will return a refresh token
+    scope: 'https://www.googleapis.com/auth/plus.me' // can be a space-delimited string or an array of scopes
+  });
+
+  console.log('Visit the url: ', url);
+  request(url, function(err, res, body){
+    if(err){
+      console.error(err);
+    }
+    console.log(res);
+  });
+  /*
+  rl.question('Enter the code here:', function (code) {
+    // request access token
+    oauth2Client.getToken(code, function (err, tokens) {
+      if (err) {
+        return callback(err);
+      }
+      // set tokens to the client
+      // TODO: tokens should be set by OAuth2 client.
+      oauth2Client.setCredentials(tokens);
+      callback();
+    });
+  });
+  */
+}
+
+// retrieve an access token
+getAccessToken(oauth2Client, function () {
+  // retrieve user profile
+  plus.people.get({ userId: 'me', auth: oauth2Client }, function (err, profile) {
+    if (err) {
+      return console.log('An error occured', err);
+    }
+    console.log(profile.displayName, ':', profile.tagline);
+  });
+});
+/*
+var post_option = {
+  blogId : _BLOGID
+}
+blogger.posts.insert({
+  blogId : _BLOGID,
+  key: _KEY,
+  title : 'test',
+  content : 'With <b>exciting</b> content...'
+}, function(err, result){
+  if(err){
+    console.error(err);
+  }
+  if(result){
+    console.log('done')
+  }
+});*/
